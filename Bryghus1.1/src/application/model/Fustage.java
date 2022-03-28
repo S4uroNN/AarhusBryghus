897ff9c4e42d33b49eb25325aa251b283880bb14
S4uroNN <alin_constantin15@hotmail.com>
2022-03-28 12:49:53 +0200

    pant

--- Bryghus1.1/src/application/model/Fustage.java
+++ Bryghus1.1/src/application/model/Fustage.java
@@ -1,7 +1,9 @@
 package application.model;
 
 public class Fustage extends Drikkevare{
+    private int pant;
     public Fustage(String navn, String størelse, String procenter) {
         super(navn, størelse, procenter);
+        pant = 200;
     }
 }
