65f452c4eedebd9e359c00fe0ebb052bab17e907
Jesper <eaajeov@students.eaaa.dk>
2022-03-31 09:54:41 +0200

    salgPane

--- Bryghus1.1/src/gui/SalgPane.java
+++ Bryghus1.1/src/gui/SalgPane.java
@@ -1,22 +1,26 @@
 package gui;
 
+import application.model.Prisliste;
 import application.model.Salg;
-import javafx.scene.control.Label;
+import application.model.Vare;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+import storage.Storage;
 
 public class SalgPane extends GridPane {
-
-
-
+    private ListView<Vare> lvwOrdre = new ListView<>();
+    private Button btnTilføjVare, btnFjernVare;
+    private ComboBox<Prisliste> prislisteComboBox;
+    private RadioButton rbDankort, rbMobilepay, rbKontant, rbRegning, rbKlippekort;
+    private RadioButton rbFastProcent, rbUserDefined, rbNoDiscount;
+    private TextField txfRabat, txfSamletPris;
+    private Storage storage = Storage.getInstance();
     public SalgPane(){
         setHgap(20);
         setVgap(10);
         setGridLinesVisible(false);
         setMaxWidth(500);
 
-        add(new Label("Morgen"), 0, 0);
-        add(new Label("Middag"), 1, 0);
-        add(new Label("Aften"), 2, 0);
-        add(new Label("Nat"), 3, 0);
+
     }
 }
