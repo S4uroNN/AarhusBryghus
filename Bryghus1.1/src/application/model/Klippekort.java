a1ea21b3f5970f1dbadec43086198c8fce2edfaa
Mathias <tidspe@gmail.com>
2022-04-04 09:34:47 +0200

    Dagsproduktion Solgteklip

--- Bryghus1.1/src/application/model/Klippekort.java
+++ Bryghus1.1/src/application/model/Klippekort.java
@@ -2,7 +2,18 @@ package application.model;
 
 public class Klippekort extends Vare{
 
+    private int klip;
+
     public Klippekort(String navn) {
         super(navn);
+        this.klip = 10;
+    }
+
+    public int getKlip() {
+        return klip;
+    }
+
+    public void setKlip(int klip) {
+        this.klip = klip;
     }
 }
